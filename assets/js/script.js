7805c017a223425fb642acc814664a56f02c1c5c
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-01-27 21:35:37 -0500

    added colour change with time functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 let pastPresentFuture = document.querySelectorAll(".description");
 const pastPresentFutureArr = Array.from(pastPresentFuture);
+console.log(pastPresentFutureArr)
 
 $("#currentDay").text(moment().format("MMMM Do YYYY, hh:mm:ss A"));
 
-// for (i=0; i < 
+for (i=0; i < pastPresentFutureArr.length; i++) {
+    let pastPresentFutureEl = pastPresentFutureArr[i];
+    let currentTime = parseInt(moment().format("k"));
+    let pastPresentFutureVal = parseInt(pastPresentFutureEl.getAttribute("id"));
+    console.log(pastPresentFutureEl.getAttribute("id"));
 
-console.log(pastPresentFutureArr)
+    if (pastPresentFutureVal == currentTime) {
+        pastPresentFutureEl.setAttribute('class', "description col-md-10 present");
+    } else if (pastPresentFutureVal < currentTime) {
+        pastPresentFutureEl.setAttribute('class', "description col-md-10 past");
+    } else if (pastPresentFutureVal > currentTime) {
+        pastPresentFutureEl.setAttribute('class', "description col-md-10 future");
+    // pastPresentFutureEl.getAttribute("id")
+
+    }
+}
