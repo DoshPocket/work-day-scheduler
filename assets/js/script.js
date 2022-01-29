965f25b401660ca13f9fbe69eaf3990ab567fbc0
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-01-28 19:25:23 -0500

    added funtionality to grab input into textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let pastPresentFuture = document.querySelectorAll(".description");
 const pastPresentFutureArr = Array.from(pastPresentFuture);
+const saveButton = document.querySelectorAll(".btn")
 console.log(pastPresentFutureArr)
+console.log(saveButton)
 
 $("#currentDay").text(moment().format("MMMM Do YYYY, hh:mm:ss A"));
 
@@ -8,7 +10,6 @@ for (i=0; i < pastPresentFutureArr.length; i++) {
     let pastPresentFutureEl = pastPresentFutureArr[i];
     let currentTime = parseInt(moment().format("k"));
     let pastPresentFutureVal = parseInt(pastPresentFutureEl.getAttribute("id"));
-    console.log(pastPresentFutureEl.getAttribute("id"));
 
     if (pastPresentFutureVal == currentTime) {
         pastPresentFutureEl.setAttribute('class', "description col-md-10 present");
@@ -16,7 +17,63 @@ for (i=0; i < pastPresentFutureArr.length; i++) {
         pastPresentFutureEl.setAttribute('class', "description col-md-10 past");
     } else if (pastPresentFutureVal > currentTime) {
         pastPresentFutureEl.setAttribute('class', "description col-md-10 future");
-    // pastPresentFutureEl.getAttribute("id")
-
     }
 }
+
+// saveButton.addEventListener
+//     saveButton.addEventListener("click", function(event) {
+//       let saveButtonVal = event.target.className.split(' ')[0];
+   
+//         console.log(saveButtonVal);
+//     });
+//      let textAreaInput = document.querySelector(".text");
+//      console.log(textAreaInput)
+ 
+// for loop for buttons
+for (i=0; i < saveButton.length; i++) {
+    let saveButtonEl = saveButton[i];
+    saveButtonEl.addEventListener("click", function(event) {
+        let buttonClassName = saveButtonEl.getAttribute("id")
+        console.log(event)
+        console.log(buttonClassName[buttonClassName.length-3]);
+    
+        let idx = buttonClassName[buttonClassName.length-2]+buttonClassName[buttonClassName.length-1];
+        console.log(idx)
+        if (idx[0] === "0") {
+            idx = idx[1]
+        }
+        let eventText = document.getElementById(idx).value;
+
+        console.log(eventText)
+   
+    })
+ 
+    let saveButtonVal = (saveButtonEl.getAttribute("id"));
+
+  
+
+};
+
+// function renderLastRegistered() {
+//     let eventText = localStorage.getItem("eventText");
+  
+//     if (!eventText) {
+//       return;
+//     }
+  
+//     // userEmailSpan.textContent = email;
+//     // userPasswordSpan.textContent = password;
+//   }
+//     saveButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     let eventText = document.querySelector("textarea").value;
+  
+//     if (eventText === "") {
+//       displayMessage("Error", "Empty event cannot be saved");
+//     } else {
+//       displayMessage("Saved successfully");
+  
+//       localStorage.setItem("eventText", eventText);
+//       renderLastRegistered();
+//     }
+// });
